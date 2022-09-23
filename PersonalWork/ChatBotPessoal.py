7ae90e2c64543a1cbce0594e1257ddb2b5975473
Lucca <luccachastinet20@gmail.com>
2022-09-23 04:51:42 -0300

    Adicionado teste pra ver se o whats-app conectou

--- PersonalWork/ChatBotPessoal.py
+++ PersonalWork/ChatBotPessoal.py
@@ -1,11 +1,20 @@
 import pandas as pd
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 pessoa = "Amanda"
-número = "******"
+numero = "******"
 mensagem = "Anti-baby"
 
 navegador = webdriver.Chrome()
 navegador.get("https://web.whatsapp.com/")
+
+while len(navegador.find_elements_by_id("side")) < 1: # Esperando o Whats-App conectar
+  time.sleep(1)
+
+# Já conectado, continua
+
+link = f"https://web.whatsapp.com/send?phone={numero}&text={mensagem}"
+
 
