c4d81516b99555732ea79171209a9192c3d05301
Lucca <luccachastinet20@gmail.com>
2022-09-23 04:40:54 -0300

    Projeto pessoal de Envio automatico de mensagem

---
+++ PersonalWork/ChatBotPessoal.py
@@ -0,0 +1,9 @@
+from sqlite3.dbapi2 import _AnyParamWindowAggregateClass
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+pessoa = "Amanda"
+número = "******"
+mensagem = "Anti-baby"
+
